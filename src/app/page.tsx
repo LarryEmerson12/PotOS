54fc8322959c41ee2d8a63ce9967a6146bbe1459
LarryEmerson12 <158800143+LarryEmerson12@users.noreply.github.com>
2025-06-01 21:01:16 +0800

    💫 Fix loading time so that images will render

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const timer = setTimeout(() => setLoading(false), 1500); // 1.5 seconds
+    const timer = setTimeout(() => setLoading(false), 3000);
     return () => clearTimeout(timer);
   }, []);
 
