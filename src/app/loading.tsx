d6143a8602bf179c5c023878bbd9c6f386caa548
LarryEmerson12 <158800143+LarryEmerson12@users.noreply.github.com>
2025-09-13 10:08:31 +0800

    ✨Update loading.tsx

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -7,6 +7,7 @@ export default function Loading() {
     <main className="bg-black w-screen h-screen flex flex-col items-center justify-center">
       <div className="grid-rows-2">
         <p>Installing PotOS</p>
+        <p>Do not shut down your computer.</p>
       </div>
     </main>
   );
