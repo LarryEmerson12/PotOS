ec30124aa243b0342dfac2769f8fee01cc51fe04
LarryEmerson12 <158800143+LarryEmerson12@users.noreply.github.com>
2025-09-13 10:05:19 +0800

    Update loading.tsx

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -7,19 +7,7 @@ export default function Loading() {
   return (
     <main className="bg-black w-screen h-screen flex flex-col items-center justify-center">
       <div className="grid-rows-2">
-        <Image
-          src="/icons/droplet.png"
-          width={50}
-          height={50}
-          alt="Droplet"
-          className="animate-bounce mx-auto"
-        />
-        <Image
-          src="/icons/potted-plant.png"
-          width={100}
-          height={100}
-          alt="Potted Plant"
-        />
+        <p>Installing PotOS</p>
       </div>
     </main>
   );
